1090d0dc5ece13cafa11c324810ac220ee9e47cf
Aaron Karr <aaron@aaronkarr.com>
2025-01-09 17:58:18 -0500

    added summary report

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def main():
     book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = word_count(text)
-    chars_dict = char_count(text)
-    print(chars_dict)
+    # text = get_book_text(book_path)
+    # num_words = word_count(text)
+    # chars_dict = char_count(text)
+    print(summary_report(book_path))
 
 def get_book_text(path):
     with open(path) as f:
@@ -23,5 +23,30 @@ def char_count(string):
         else:
             char_dict[char] = 1
     return char_dict
+
+def summary_report(path):
+    text = get_book_text(path)
+    num_words = word_count(text)
+    char_dict = char_count(text)
+    char_list = []
+
+    for key in char_dict:
+        if key.isalpha():
+            char_list.append({"letter": key, "count": char_dict[key]})
+
+    char_list = sorted(char_list, reverse=True, key=lambda x: x["count"])
+
+    report = ""
+    title = f"--- Begin report of {path} ---\n"
+    word_count_report = f"{num_words} found in the document\n\n"
+    report += title
+    report += word_count_report
+
+    for i in range(0, len(char_list)):
+        char_count_report = f"The '{char_list[i]["letter"]}' character was found {char_list[i]["count"]} times\n"
+        report += char_count_report
+    report += "--- End of report ---"
+
+    return report
 
 main()
